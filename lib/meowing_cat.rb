45b7b301aa925e3b8a8539734b8b1d3fe0b5e7fe
root <gloria.kitili@student.moringaschool.com>
2023-02-18 15:04:28 +0300

    added macro attr_acccessor

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,11 +1,30 @@
 ## code your solution here. 
-class class 
+class Cat
     attr_accessor :name
     def meow
         puts "meow!"
     end
-
 end
-
 pussy_cat = Cat.new
-pussy_cat.meow
+pussy_cat.meow 
+
+
+
+
+# class Cat 
+#     attr_reader :name
+
+#      def name 
+#         @name
+#      end
+
+#     attr_writer :name 
+#     def name = name
+#         @name = name 
+#     end 
+# end
+
+# pussy_cat = Cat.new 
+# pussy_cat.name = "meow"
+# pussy_cat.meow = "Tailand"
+
